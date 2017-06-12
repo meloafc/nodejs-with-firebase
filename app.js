a8e9025837dfc1926b93dd35c4d478dd56355e05
Alex Fabiano <afabiano15@gmail.com>
2017-06-11 21:02:11 -0400

    atualizando modelo

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
 //ativarLog(firebase);
-lerEvento("01/06/2017","12/06/2017");
+//quantidadeDeEventos();
+lerEvento("01/06/2017","13/06/2017");
 
 
 function lerEvento(inicio, fim) {
@@ -14,49 +15,29 @@ function lerEvento(inicio, fim) {
   let fimUnix = getUnixTimestamp(fim);
 
   let telaLigada = 0;
-  let ultimoEvento;
 
-  let ref = db.ref("events");
-  ref.orderByChild("date")
+  let ref = db.ref("events/active_screen");
+  ref.orderByChild("initialDate")
     .startAt(inicioUnix)
     .endAt(fimUnix)
     .once("value", function(snapshot) {
       snapshot.forEach(function(childSnap){
         let event = childSnap.val();
 
-        let log = event.type;
-        log = log + " - " + moment(event.date).format("DD/MM/YYYY HH:mm:ss.SSS");
-        
-        if(ultimoEvento != undefined) {
-          if(ultimoEvento.type == 'ACTION_SCREEN_ON') {
-            if(ultimoEvento.date < event.date) {
-              let tempoTelaLigada = event.date - ultimoEvento.date;
-              telaLigada = telaLigada + tempoTelaLigada;
-              log = log + " duração:" + formatarMilisegundos(tempoTelaLigada);
-            }
-          }
-        }
+        let log = moment(event.initialDate).format("DD/MM/YYYY HH:mm:ss.SSS");
+        log = log + " duração:" + formatarMilisegundos(event.duration);
+
+        telaLigada = telaLigada + event.duration;
 
         console.log(log);
-        ultimoEvento = event;
       });
   }).then(function(result) {
     console.log(formatarMilisegundos(telaLigada));
   });  
 }
 
-function lerTodosOsEventos() {
-  let ref = db.ref("events");
-
-  ref.once('value', function(snapshot) {
-      snapshot.forEach(function(childSnap){
-        console.log(childSnap.val());
-      });
-  });
-}
-
 function quantidadeDeEventos() {
-  let ref = db.ref("events");
+  let ref = db.ref("events/active_screen");
 
   ref.once('value', function(snapshot) {
     console.log(snapshot.numChildren());
